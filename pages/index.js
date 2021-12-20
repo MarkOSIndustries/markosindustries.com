6ba4244c503860c0d2df1b25f41b13a36d79a21f
Mark Green <markosindustries@gmail.com>
2021-12-21 00:29:48 +1100

    Just adding some simple content. Ugly but functional.

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,21 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>MarkOSIndustries</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="MarkOSIndustries" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Just some bloke who enjoys coding, and the colour black.
         </p>
+        <p /><p />
+        <h2>Projects on GitHub</h2>
+        <ul>
+          <li><b><a href="https://github.com/MarkOSIndustries/monosodium-glutamate">Monosodium Glutamate</a></b><br/>A set of tools for working with kafka, protobuf, GRPC, parquet files, and other stuff I've found useful working on distributed systems</li>
+          <li><b><a href="https://github.com/MarkOSIndustries/distroboy">DistroBoy</a></b><br/>An attempt to simplify distributed data processing.</li>
+        </ul>
       </main>
 
       <Footer />
